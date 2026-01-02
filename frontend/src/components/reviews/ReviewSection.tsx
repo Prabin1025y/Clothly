de97cac2fa2a20e8a408a7b4309b56375c4f6400
Prabin1025y <acharyaprabin1025y@gmail.com>
2026-01-02 20:52:30 +0545

    feat: enhance error handling in ReviewSection with user feedback and retry option

--- frontend/src/components/reviews/ReviewSection.tsx
+++ frontend/src/components/reviews/ReviewSection.tsx
@@ -1,6 +1,10 @@
 import ReviewForm from "./ReviewForm"
-import { useGetReviews } from "@/hooks/useReviews"
+import { reviewKeys, useGetReviews } from "@/hooks/useReviews"
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
 import ReviewCard from "./ReviewCard"
+import { AlertOctagon, Loader2 } from "lucide-react";
+import { Button } from "../ui/button";
+import { useQueryClient } from "@tanstack/react-query";
 
 // export interface Comment {
 //     id: string
@@ -22,7 +26,8 @@ interface ReviewSectionProps {
 
 export default function ReviewSection({ productId }: ReviewSectionProps) {
 
-    const { data, isLoading, isError, error } = useGetReviews(productId);
+    const { data, isLoading, isError, error, isFetching } = useGetReviews(productId);
+    const queryClient = useQueryClient();
 
 
     return (
@@ -31,8 +36,32 @@ export default function ReviewSection({ productId }: ReviewSectionProps) {
             <div className="flex flex-col gap-4 max-h-96 overflow-y-auto pr-2">
                 {(
                     () => {
-                        if (isError)
-                            return <div>Error...</div>
+                        if (isError) {
+                            console.error(error);
+                            return <Empty >
+                                <EmptyHeader>
+                                    <EmptyMedia variant="icon">
+                                        <AlertOctagon color="red" />
+                                    </EmptyMedia>
+                                    <EmptyTitle className="text-red-500">An Error Occured!!</EmptyTitle>
+                                    <EmptyDescription className="text-red-400">
+                                        An error occured while fetching products. Please try again!!
+                                    </EmptyDescription>
+                                </EmptyHeader>
+                                <EmptyContent>
+                                    <div className="flex gap-2">
+                                        {(isFetching && !isLoading) ? <Button disabled className="bg-red-500">
+                                            <Loader2 className="animate-spin" /> Retrying...
+                                        </Button> : <Button
+                                            className="cursor-pointer bg-red-500"
+                                            onClick={() => queryClient.invalidateQueries({ queryKey: reviewKeys.list(productId) })}
+                                        >
+                                            Retry
+                                        </Button>}
+                                    </div>
+                                </EmptyContent>
+                            </Empty>
+                        }
 
                         if (isLoading || !data) {
                             console.error(error);
